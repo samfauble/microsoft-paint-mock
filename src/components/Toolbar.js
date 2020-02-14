79b044e0865df01a3388f91426e8c733cce623e0
samfauble <53917130+samfauble@users.noreply.github.com>
2020-02-14 13:12:59 -0400

    implemented component hierarchy

--- src/components/Toolbar.js
+++ src/components/Toolbar.js
@@ -1,14 +1,10 @@
 import React from 'react'
 import Tools from './Tools'
-import ColorSelector from './ColorSelector'
-import CanvasSize from './CanvasSize'
 
 export default function Toolbar() {
     return (
         <div>
             <Tools />
-            <ColorSelector />
-            <CanvasSize />
         </div>
     )
 }
--- src/components/Tools.js
+++ src/components/Tools.js
@@ -3,7 +3,7 @@ import React from 'react'
 function Tools() {
     return (
         <div>
-            
+
         </div>
     )
 }
